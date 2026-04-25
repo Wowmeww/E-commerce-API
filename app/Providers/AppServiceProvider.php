4a60a992e2a34a526171bbbf6d059308f627ee0e
Nico Bernard B. Firmanes <nicobernardfirmanes@gmail.com>
2026-04-25 18:22:48 +0800

    fix(email verification redirects): email verification logic in AppServiceProvider and EmailVerificationService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +27,39 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         Model::preventLazyLoading();
+
+        $this->overridePasswordResetUrl();
+        $this->overrideEmailVerificationUrl();
+    }
+
+
+
+    private function overridePasswordResetUrl(): void
+    {
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token): string {
+            return config('app.frontend_url') . '/reset-password'
+                . '?token=' . $token
+                . '&email=' . urlencode($notifiable->getEmailForPasswordReset());
+        });
+    }
+
+    private function overrideEmailVerificationUrl(): void
+    {
+        VerifyEmail::createUrlUsing(function (object $notifiable): string {
+            $verifyUrl = URL::temporarySignedRoute(
+                'verification.verify',
+                Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+                [
+                    'id' => $notifiable->getKey(),
+                    'hash' => sha1($notifiable->getEmailForVerification()),
+                ]
+            );
+
+            return str_replace(
+                config('app.url'),
+                config('app.frontend_url'),
+                $verifyUrl
+            );
+        });
     }
 }
